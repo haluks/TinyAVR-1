52fb60709b789314dd51d33e42d8054fff44d3ca
Haluk Simsek <37951127+haluks@users.noreply.github.com>
2024-03-21 15:31:26 +0300

    Add files via upload

---
+++ tiny1_uart/tiny1_uart.c
@@ -0,0 +1,92 @@
+/*
+ * tiny1_uart.c
+ *
+ * Created: 12.03.2024 11:10:58
+ * Author : haluk
+ */ 
+
+
+#include "tiny1_uart.h"
+
+#if (UART_Rx_Boyut>256)
+volatile uint16_t rx_bas=0,rx_son=0;
+#else
+volatile uint8_t rx_bas=0,rx_son=0;
+#endif
+#if (UART_Tx_Boyut>256)
+volatile uint16_t tx_bas=0,tx_son=0;
+#else
+volatile uint8_t tx_bas=0,tx_son=0;
+#endif
+volatile uint8_t rx_ring[UART_Rx_Boyut];
+volatile uint8_t tx_ring[UART_Tx_Boyut];
+volatile uint8_t uaflag=0;
+
+ISR (USART0_RXC_vect){
+	uint8_t gelen=USART0.RXDATAL;	
+	rx_bas=(rx_bas+1) & UART_Rx_Mask;
+	rx_ring[rx_bas]=gelen;	
+	if (gelen=='\n'){
+		uaflag++;
+	}
+}
+ISR (USART0_DRE_vect){	//uart veri gönderim kesmesi
+	tx_son=(tx_son+1)&UART_Tx_Mask;
+	USART0.TXDATAL=tx_ring[tx_son];
+	if (tx_son==tx_bas)
+	UART_Bos_Off;
+}
+ISR(USART0_TXC_vect){
+	
+}
+void uart_basla(Bd_rate_t _baud){
+	cli();	
+	PORTB.DIRSET|=(1<<UART_TX_PIN);
+	PORTB.OUTSET|=(1<<UART_TX_PIN);
+	PORTB.DIRSET&=~(0<<UART_RX_PIN);
+	uint16_t baudRate=0;
+	baudRate=(float)(F_CPU*4/_baud);
+	USART0.CTRLB|=USART_RXMODE_NORMAL_gc;
+	if (_baud>=115200){//115200 ve üstünde U2X 1 yapýlýyor.
+		baudRate=(float)(F_CPU*8/_baud);
+		USART0.CTRLB|=USART_RXMODE_CLK2X_gc;
+	}
+	//USART0.BAUD=baudRate;
+	USART0.BAUD=(baudRate>>8);
+	USART0.BAUD=baudRate;
+	USART0.CTRLA|=(1<<USART_RXCIE_bp)|(1<<USART_TXCIE_bp);
+	USART0.CTRLB|=(1<<USART_RXEN_bp)|(1<<USART_TXEN_bp);
+	USART0.CTRLC|=USART_CHSIZE_8BIT_gc;
+	sei();
+}
+uint8_t uart_oku(){
+	rx_son=(rx_son+1) & UART_Rx_Mask;
+	return rx_ring[rx_son];
+}
+void uart_gonder(uint8_t uData){
+	tx_bas=(tx_bas+1)&UART_Tx_Mask;
+	while ((tx_son==tx_bas));
+	tx_ring[tx_bas]=uData;
+	UART_Bos_On;
+}
+void uart_dizi(const char *str){
+	while(*str){
+		tx_bas=(tx_bas+1)&UART_Tx_Mask;
+		while ((tx_son==tx_bas));
+		tx_ring[tx_bas]=*str++;
+	}
+	UART_Bos_On;
+}
+uint8_t uart_gelen(){
+	if ((uaflag>0)&&!(rx_son==rx_bas))
+	return 1;	
+	return 0;
+}
+void uart_dizi_al(char *stri){
+	uint8_t poz=0;
+	do{		
+		stri[poz++]=uart_oku();
+	} while (!(rx_bas==rx_son));		
+	stri[poz]='\0';
+	uaflag=0;
+}
---
+++ tiny1_uart/tiny1_uart.h
@@ -0,0 +1,42 @@
+/*
+ * tiny1_uart.c
+ *
+ * Created: 12.03.2024 11:10:58
+ * Author : haluk
+ */ 
+#ifndef TINY1_UART_H
+#define TINY1_UART_H
+
+
+#define F_CPU 3333333 /* 20MHz / 6(default prescale) */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/////////////////////////////////
+#define UART_Rx_Boyut			128
+#define UART_Tx_Boyut			128
+#define UART_Rx_Mask			(UART_Rx_Boyut-1)
+#define UART_Tx_Mask			(UART_Tx_Boyut-1)
+#define UART_Bos_On				USART0.CTRLA|=(1<<USART_DREIE_bp)// uart UDR register boþ kesmesi
+#define UART_Bos_Off			USART0.CTRLA&=~(1<<USART_DREIE_bp)// uart UDR register boþ kesmesi
+#define PORTB2					PIN2_bp
+#define PORTB3					PIN3_bp
+#define UART_TX_PIN				PORTB2
+#define UART_RX_PIN				PORTB3
+typedef enum{
+	Bd4800			=4800,
+	Bd9600			=9600,
+	Bd19200			=19200,
+	Bd38400			=38400,
+	Bd57600			=57600,
+	Bd115200		=115200,
+}Bd_rate_t;
+////////////////////////////////////////////////////
+void uart_basla(Bd_rate_t _baud);
+uint8_t uart_oku();
+void uart_gonder(uint8_t uData);
+void uart_dizi(const char *str);
+uint8_t uart_gelen();
+void uart_dizi_al( char *stri);
+
+#endif
