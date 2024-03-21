98064d326d22cf7f7266185300b1339315789ef8
Haluk Simsek <37951127+haluks@users.noreply.github.com>
2024-03-21 15:36:16 +0300

    Add files via upload

---
+++ tiny1_i2c_master/tiny1_i2c_master.c
@@ -0,0 +1,172 @@
+
+
+
+/*
+ * tiny1_i2c_master.h
+ *
+ * Created: 12.03.2024 11:05:21
+ *  Author: haluk
+ */ 
+
+
+#include "tiny1_i2c_master.h"
+#include <util/delay.h>
+static volatile uint8_t i2c_rx_bas=0,i2c_rx_son=0,i2c_tx_bas=0,i2c_tx_son=0,i2c_rx_len=0;
+static volatile uint8_t i2c_rx_ring[I2C_Rx_Boyut];
+static volatile uint8_t i2c_tx_ring[I2C_Tx_Boyut];
+static volatile uint8_t Sladr_RW=0, i2c_rep=0;
+static volatile uint8_t i2c_state=0;
+volatile uint32_t timeout=0;
+volatile uint8_t adres_nack=0;
+void i2c_init(){	
+	cli();
+	TWI0.MBAUD=TWI_MBAUD(F_SCL,200);	
+	TWI0.MCTRLA|=TWI_ENABLE_bm/*|(1<<TWI_SMEN_bp)|(TWI_TIMEOUT_200US_gc)|(1<<TWI_QCEN_bp)*/|TWI_WIEN_bm|TWI_RIEN_bm;
+	I2C_STATUS|= TWI_RIF_bm | TWI_WIF_bm|TWI_BUSSTATE_IDLE_gc;
+	sei();
+}
+void i2c_disable(){
+	TWI0.MCTRLB=TWI_FLUSH_bm;
+	TWI0.CTRLA=0x00;	
+}
+void i2c_reset(){
+	i2c_disable();
+	i2c_init();	
+}
+void i2c_start( ){	
+	TWI0.MADDR=Sladr_RW;	
+}
+void i2c_adr(uint8_t adr,i2c_cmd_t cmd){
+		while(!(I2C_STATUS & TWI_BUSSTATE_IDLE_gc)){
+			timeout++;
+			if (timeout>I2C_TIMEOUT){
+				timeout=0;
+				i2c_reset();
+				return;
+			}
+		}
+		i2c_tx_bas=0;
+		Sladr_RW=((adr<<1)|cmd);
+		i2c_state=cmd;	
+}
+void i2c_data(uint8_t data){
+	i2c_tx_ring[i2c_tx_bas++]=data;
+}
+void i2c_end( i2c_rep_t repst){
+	i2c_rep=repst;
+	i2c_tx_son=0;	
+	i2c_start();
+}
+void i2c_stop(){
+	TWI0.MCTRLB|=TWI_MCMD_STOP_gc;
+	
+	while(!(I2C_STATUS & TWI_BUSSTATE_IDLE_gc)){
+	timeout++;
+	if (timeout>I2C_TIMEOUT){
+		timeout=0;
+		i2c_reset();
+		return;
+		}
+	}	
+	TWI0.MCTRLB=0x00;
+}
+void i2c_send_data(uint8_t adr, uint8_t data,  i2c_rep_t repst){	
+	
+	i2c_adr(adr, I2C_WRITE);
+	i2c_data(data);
+	i2c_end(repst);
+}
+void i2c_send(uint8_t adr, uint8_t* str, uint8_t len,  i2c_rep_t repst){
+	
+	i2c_adr(adr, I2C_WRITE);
+	i2c_tx_bas=0;
+	for (uint8_t i=0;i<len;i++){
+		i2c_data( str[i]);
+	}
+	i2c_end(repst);
+}
+void i2c_send_str(uint8_t adr, const char* str,  i2c_rep_t repst){
+	i2c_adr(adr, I2C_WRITE);
+	i2c_tx_bas=0;
+	while (*str){
+		i2c_data(*str++);		
+	}	
+	i2c_end(repst);	
+}
+void i2c_read(uint8_t adr, uint8_t len){	
+	i2c_rx_bas=0;
+	i2c_rx_son=0;
+	i2c_rx_len=len;
+	i2c_adr(adr, I2C_READ);	
+	i2c_start();
+}
+uint8_t i2c_gelen(){	
+
+	if (i2c_rx_son==i2c_rx_bas)	return 0;
+	return 1;	
+}
+uint8_t i2c_oku(){		
+	return i2c_rx_ring[i2c_rx_son++];
+}
+uint8_t i2c_adres_scan(){
+	for (uint8_t i=0;i<128;i++){
+		adres_nack=1;
+		i2c_send_data(i,0x00,N_REPEAT);
+		_delay_ms(10);
+		if (adres_nack)
+		return i;
+	}
+	return 0;
+}
+ISR (TWI0_TWIM_vect){
+	i2c_state|=(I2C_STATUS&0xD0);
+	switch (i2c_state){
+		case I2C_MTR_ADR_ACK://adres yazýldý ack geldi
+			i2c_state= STAT_WRT_DATA;
+		case I2C_MTR_DATA_ACK://data yazýldý ack geldi
+			if (i2c_tx_son!=i2c_tx_bas){
+				TWI0.MDATA=i2c_tx_ring[i2c_tx_son++];
+			}
+			else if (i2c_rep==1){
+				i2c_rep=0;
+				TWI0.MCTRLB|=TWI_MCMD_REPSTART_gc;
+			}
+			else{
+				i2c_stop();
+			}		
+		break;
+		case I2C_MTR_ADR_NACK://adres yazýldý nack geldi
+		adres_nack=0;
+		i2c_stop();
+		break;
+		case I2C_MTR_DATA_NACK:	//data yazýldý nack geldi
+		i2c_stop();
+		break;	
+		case I2C_MRD_DATA_ACK://okuma data yazýldý ack geldi	
+					
+		case I2C_MRD_ADR_ACK://okuma adres yazýldý ack geldi
+		i2c_state=STAT_RD_DATA;	
+		
+		i2c_rx_ring[i2c_rx_bas++]=TWI0.MDATA;
+			if (i2c_rx_bas!=i2c_rx_len){	
+				TWI0.MCTRLB |= TWI_ACKACT_ACK_gc|TWI_MCMD_RECVTRANS_gc;
+				//TWI0.MCTRLB |= TWI_MCMD_RECVTRANS_gc;
+				}else{
+				i2c_state=I2C_MRD_DATA_NACK;						
+				TWI0.MCTRLB |= TWI_ACKACT_NACK_gc;
+				
+				i2c_stop();
+			}
+		break;
+		case I2C_MRD_ADR_NACK://okuma adres yazýldý ack geldi
+			i2c_stop();
+		break;	
+		case I2C_MRD_DATA_NACK:		
+			i2c_stop();
+		break;
+		default:
+			i2c_stop();
+		break;
+	}
+	
+}
---
+++ tiny1_i2c_master/tiny1_i2c_master.h
@@ -0,0 +1,74 @@
+
+
+
+/*
+ * tiny1_i2c_master.h
+ *
+ * Created: 12.03.2024 11:05:21
+ *  Author: haluk
+ */ 
+
+#ifndef TINY1_I2C_MASTER_H
+#define TINY1_I2C_MASTER_H
+
+#define F_CPU 3333333
+#define F_SCL 100000
+#define TWI_MBAUD(F_SCL,Trise) ((F_CPU/F_SCL)/2)-(5+(((F_CPU/1000000)*Trise)/2000))
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define I2C_Rx_Boyut					16
+#define I2C_Tx_Boyut					16
+#define I2C_Rx_Mask						(I2C_Rx_Boyut-1)
+#define I2C_Tx_Mask						(I2C_Tx_Boyut-1)
+#define I2C_TIMEOUT						1000
+//I2C status
+#define I2C_STATUS						TWI0.MSTATUS
+//I2C master status
+#define I2C_MTR_ADR_ACK					0x40// adres yazýldý ack geldi
+#define I2C_MTR_ADR_NACK				0x50// adres yazýldý nack geldi
+#define I2C_MTR_DATA_ACK				0x42// veri gitti ack geldi
+#define I2C_MTR_DATA_NACK				0x52// veri gitti nack geldi
+#define I2C_MRD_ADR_ACK					0x81//adres yazýldý ack geldi
+#define I2C_MRD_ADR_NACK				0x91// adres yazýldý nack geldi
+#define I2C_MRD_DATA_ACK				0x83// veri alýndý ack gönderildi
+#define I2C_MRD_DATA_NACK				0x93//veri alýndý nac gönderildi
+
+//I2C error status
+#define I2C_ARB_LOST					TWI_ARBLOST_bm//hat yönetim kaybý
+#define I2C_UNKNOWN						TWI_BUSSTATE_UNKNOWN_gc//belirsiz durum
+#define I2C_BUS_ERR						TWI_BUSERR_bm//hat hatasý
+
+//////////////////////////////////////////////////////////////////////////
+
+typedef enum {
+	I2C_WRITE		=0,
+	I2C_READ		=1,
+}i2c_cmd_t;
+typedef  enum {
+	_REPEAT			=1,
+	N_REPEAT		=0,
+}i2c_rep_t;
+
+#define	STAT_WRT_ADDR	0
+#define	STAT_RD_ADDR	1
+#define	STAT_WRT_DATA	2
+#define	STAT_RD_DATA	3
+
+void i2c_init();
+void i2c_disable();
+void i2c_reset();
+void i2c_start();
+void i2c_stop();
+void i2c_adr(uint8_t adr,i2c_cmd_t cmd);
+void i2c_data(uint8_t data);
+void i2c_end( i2c_rep_t repst);
+void i2c_send_data(uint8_t adr, uint8_t data,  i2c_rep_t repst);
+void i2c_send(uint8_t adr, uint8_t* str,uint8_t len,  i2c_rep_t repst);
+void i2c_send_str(uint8_t adr, const char* str,  i2c_rep_t repst);
+void i2c_read(uint8_t adr, uint8_t len);
+uint8_t i2c_gelen();
+uint8_t i2c_oku();
+uint8_t i2c_adres_scan();
+
+#endif
